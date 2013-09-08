ae537d8faa93abc0d75664d2968cc903aced408a
Kaique da Silva <kaique.developer@gmail.com>
2013-09-08 02:01:12 -0300

    Decode in progress

--- test/morse.test.js
+++ test/morse.test.js
@@ -3,21 +3,88 @@ var morse = require('../lib/morse');
 
 suite('Morse code', function () {
     suite('letters', function () {    
-        var actual = morse.encode('hello world');
-        var expected = [
-            '. . . .', ' ',
-            '.', ' ',
-            '. - . .', ' ',
-            '. - . .', ' ',
-            '- - -', ' ',
-            '      ',
-            '. - -', ' ',
-            '- - -', ' ',
-            '. - .', ' ',
-            '. - . .', ' ',
-            '- . .'
-        ].join('');
+        test('encode', function () {
+            var actual = morse.encode('hello world');
+            var expected = [
+                '. . . .', ' ',
+                '.', ' ',
+                '. - . .', ' ',
+                '. - . .', ' ',
+                '- - -', ' ',
+                '      ',
+                '. - -', ' ',
+                '- - -', ' ',
+                '. - .', ' ',
+                '. - . .', ' ',
+                '- . .'
+            ].join('');
 
-        actual.should.be.eql( expected );
+            ( typeof( actual ) ).should.be.eql( typeof( expected ) );
+            actual.should.be.have.lengthOf( expected.length );
+            actual.should.be.eql( expected );
+        });
+
+        test('decode char', function () {
+            var actual = morse.decodeChar([
+                '. . . .',
+            ].join(''));
+            var expected = 'h';
+
+            ( typeof( actual ) ).should.be.eql( typeof( expected ) );
+            actual.should.be.have.lengthOf( expected.length );
+            actual.should.be.eql( expected );
+        });
+
+        test('extract', function () {
+            var actual =  morse.extract([
+                '. . . .', ' ',
+                '.', ' ',
+                '. - . .', ' ',
+                '. - . .', ' ',
+                '- - -', ' ',
+                '      ',
+                '. - -', ' ',
+                '- - -', ' ',
+                '. - .', ' ',
+                '. - . .', ' ',
+                '- . .'
+            ].join(''));   
+            var expected = [
+                '. . . .',
+                '.',
+                '. - . .',
+                '. - . .',
+                '- - -',
+                '      ',
+                '. - -',
+                '- - -',
+                '. - .',
+                '. - . .',
+                '- . .'
+            ];
+
+            actual.should.be.eql(expected);
+        });
+
+        test('decode', function () {
+            var actual = morse.decode([
+                '. . . .', ' ',
+                '.', ' ',
+                '. - . .', ' ',
+                '. - . .', ' ',
+                '- - -', ' ',
+                '      ',
+                '. - -', ' ',
+                '- - -', ' ',
+                '. - .', ' ',
+                '. - . .', ' ',
+                '- . .'
+            ].join(''));
+            var expected = 'hello world';
+
+            ( typeof( actual ) ).should.be.eql( typeof( expected ) );
+            actual.should.be.have.lengthOf( expected.length );
+            actual.should.be.eql( expected );
+        });
     });    
 });
